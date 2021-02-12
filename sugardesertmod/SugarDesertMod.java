546fa215d6197c045185d72ff7bf6ec737500180
soramame-cycles <62301498+soramame-cycles@users.noreply.github.com>
2021-02-12 20:37:20 +0900

    Add files via upload

--- sugardesertmod/SugarDesertMod.java
+++ sugardesertmod/SugarDesertMod.java
@@ -1,130 +1,65 @@
 package com.sugar.sugardesertmod;
 
+import java.util.logging.Logger;
+
 import com.sugar.sugardesertmod.gen.biome.ModBiome;
 import com.sugar.sugardesertmod.gen.block.BismuthOreGen;
 import com.sugar.sugardesertmod.gen.block.EndDiaGen;
 import com.sugar.sugardesertmod.gen.block.NetherDiaGen;
 import com.sugar.sugardesertmod.gen.block.SugarBlockGen;
 import com.sugar.sugardesertmod.gen.block.SugarDiaOreGen;
-import com.sugar.sugardesertmod.init.SugarDBlock;
-import com.sugar.sugardesertmod.items.SDItem;
-import com.sugar.sugardesertmod.linkmod.OreDictionaryHandler;
+import com.sugar.sugardesertmod.handlers.OreDictionaryHandler;
+import com.sugar.sugardesertmod.init.block.SugarDBlock;
+import com.sugar.sugardesertmod.init.item.SDItem;
+import com.sugar.sugardesertmod.init.tool.SugarDTools;
 import com.sugar.sugardesertmod.recipes.SmeltingSugarD;
 import com.sugar.sugardesertmod.tab.CreativeTabsSugarD;
-import com.sugar.sugardesertmod.tools.SugarDTools;
 
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.init.MobEffects;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemFood;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.registries.IForgeRegistry;
 
-@Mod(modid = SugarDesertMod.MOD_ID,name = SugarDesertMod.MOD_NAME,version = SugarDesertMod.MOD_VERSION)
+@Mod(modid = SugarDesertMod.MOD_ID,name = SugarDesertMod.MOD_NAME,version = SugarDesertMod.MOD_VERSION,
+	updateJSON = "https://raw.githubusercontent.com/soramame-cycles/sugar_desertmod/master/sugarD_updateChecker.json")
 public class SugarDesertMod {
 	public static final String MOD_ID = "sugard";
 	public static final String MOD_NAME = "SugerDesertmod";
-	public static final String MOD_VERSION = "1.6";
+	public static final String MOD_VERSION = "1.6.2";
 	public static CreativeTabsSugarD Tab_sugard = new CreativeTabsSugarD();
 
-	public static Item bismuth_crystal;
-	public static Item bismuth_liquid;
-	public static Item Sugar_diamond;
-	public static Item Sugar_diamond_nugget;
-	public static Item Azuki_been;
-	public static Item Azuki_paste;
-	public static Item Azuki_been_bun;
-	public static Item Poppy_seed;
+	public static Logger logger;
+
+	@Instance(MOD_ID)
+	public static SugarDesertMod instance;
 
 	@Mod.EventHandler
+	//最初に処理をしたいものをここにいれる
 	public void preinit(FMLPreInitializationEvent event) {
 
-		bismuth_crystal = new Item()
-				.setCreativeTab(Tab_sugard)
-				.setUnlocalizedName("bismuth_crystal")
-				.setRegistryName("bismuth_crystal")
-				.setMaxStackSize(64);
-
-		bismuth_liquid = new Item()
-				.setCreativeTab(Tab_sugard)
-				.setUnlocalizedName("bismuth_liquid")
-				.setRegistryName("bismuth_liquid")
-				.setMaxStackSize(64);
-
-		Sugar_diamond = new Item()
-				.setCreativeTab(Tab_sugard)
-				.setUnlocalizedName("sugar_diamond")
-				.setRegistryName("sugar_diamond");
-
-		Sugar_diamond_nugget = new Item()
-				.setCreativeTab(Tab_sugard)
-				.setUnlocalizedName("sugar_diamond_nugget")
-				.setRegistryName("sugar_diamond_nugget");
-
-		Azuki_been = new Item()
-				.setCreativeTab(Tab_sugard)
-				.setRegistryName("azuki_been")
-				.setUnlocalizedName("azuki_been");
-
-		Azuki_paste = new ItemFood(4, 0.3F, true)
-				.setCreativeTab(Tab_sugard)
-				.setUnlocalizedName("azuki_paste")
-				.setRegistryName("azuki_paste");
-
-		Azuki_been_bun = ((ItemFood) new ItemFood(8,0.6F,true)
-				.setCreativeTab(Tab_sugard)
-				.setUnlocalizedName("azuki_been_bun")
-				.setRegistryName("azuki_been_bun"))
-				.setPotionEffect(new PotionEffect(MobEffects.HASTE, 1000, 0), 1.0F);
-
-		Poppy_seed = new Item()
-				.setCreativeTab(Tab_sugard)
-				.setUnlocalizedName("poppy_seed")
-				.setRegistryName("poppy_seed");
-
-		ForgeRegistries.ITEMS.register(bismuth_crystal);
-		ForgeRegistries.ITEMS.register(bismuth_liquid);
-		ForgeRegistries.ITEMS.register(Sugar_diamond);
-		ForgeRegistries.ITEMS.register(Sugar_diamond_nugget);
-		ForgeRegistries.ITEMS.register(Azuki_been);
-		ForgeRegistries.ITEMS.register(Azuki_paste);
-		ForgeRegistries.ITEMS.register(Azuki_been_bun);
-		ForgeRegistries.ITEMS.register(Poppy_seed);
-
-		if(event.getSide().isClient()) {
-			ModelLoader.setCustomModelResourceLocation(bismuth_crystal, 0, new ModelResourceLocation("sugard:bismuth_crystal"));
-			ModelLoader.setCustomModelResourceLocation(bismuth_liquid, 0, new ModelResourceLocation("sugard:bismuth_liquid"));
-			ModelLoader.setCustomModelResourceLocation(Sugar_diamond, 0, new ModelResourceLocation("sugard:sugar_diamond"));
-			ModelLoader.setCustomModelResourceLocation(Sugar_diamond_nugget, 0, new ModelResourceLocation("sugard:sugar_diamond_nugget"));
-			ModelLoader.setCustomModelResourceLocation(Azuki_been, 0, new ModelResourceLocation("sugard:azuki_been"));
-			ModelLoader.setCustomModelResourceLocation(Azuki_paste, 0, new ModelResourceLocation("sugard:azuki_paste"));
-			ModelLoader.setCustomModelResourceLocation(Azuki_been_bun, 0, new ModelResourceLocation("sugard:azuki_been_bun"));
-			ModelLoader.setCustomModelResourceLocation(Poppy_seed, 0, new ModelResourceLocation("sugard:poppy_seed"));
-		}
 	}
 
 	@Mod.EventHandler
-	//多分外部化のコード
+	//このjava自体をイベントとして扱うコード
 	public void construct(FMLConstructionEvent event) {
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	@Mod.EventHandler
-	//鉱石生成だと思う
+	//preInitの次に読み込ませたいもの
 	public void init(FMLInitializationEvent event) {
 		GameRegistry.registerWorldGenerator(new BismuthOreGen(), 0);
 		GameRegistry.registerWorldGenerator(new SugarBlockGen(), 0);
@@ -137,7 +72,7 @@ public class SugarDesertMod {
 	}
 
 	@SubscribeEvent
-	//多分アイテム登録のイベント
+	//アイテム登録のイベント
 	public void registerItems(RegistryEvent.Register<Item> event) {
 		SugarDBlock.Bismuth_Ore(event);
 		SugarDBlock.Bismuth_crystal_block(event);
@@ -173,6 +108,9 @@ public class SugarDesertMod {
 		SugarDBlock.Sugar_cob_stairs(event);
 		SugarDBlock.Sugar_planks_button(event);
 		SugarDBlock.Sugar_stone_button(event);
+		SugarDBlock.Cookie_block(event);
+		SugarDBlock.Raw_sugar_block(event);
+		SugarDBlock.Moist_sugar_block(event);
 		SugarDTools.Bismuth_pickaxe(event);
 		SugarDTools.Sugar_diamond_axe(event);
 		SugarDTools.Sugar_diamond_hoe(event);
@@ -180,12 +118,28 @@ public class SugarDesertMod {
 		SugarDTools.Sugar_diamond_shovel(event);
 		SugarDTools.Sugar_diamond_sword(event);
 		SDItem.Pudding(event);
+		SDItem.Azuki_been(event);
+		SDItem.Azuki_paste(event);
+		SDItem.Azuki_been_bun(event);
+		SDItem.Poppy_seed(event);
+		SDItem.bismuth_crystal(event);
+		SDItem.bismuth_liquid(event);
+		SDItem.Sugar_diamond(event);
+		SDItem.Sugar_diamond_nugget(event);
+		SDItem.Crushed_Sugar_Cane(event);
+		SDItem.Raw_Sugar(event);
 
 
 	}
 
 	@SubscribeEvent
-	//多分モデル登録のイベント
+	//blockの登録イベント
+	public void registerBlocks(RegistryEvent.Register<Block> event) {
+
+	}
+
+	@SubscribeEvent
+	//モデル登録のイベント
 	@SideOnly(Side.CLIENT)
 	public void registerModels(ModelRegistryEvent event) {
 		SugarDBlock.Bismuth_OreModel(event);
@@ -222,6 +176,9 @@ public class SugarDesertMod {
 		SugarDBlock.Sugar_cob_stairsModel(event);
 		SugarDBlock.Sugar_planks_buttonModel(event);
 		SugarDBlock.Sugar_stone_buttonModel(event);
+		SugarDBlock.Cookie_blockModel(event);
+		SugarDBlock.Raw_sugar_blockModel(event);
+		SugarDBlock.Moist_sugaar_blockModel(event);
 		SugarDTools.Bismuth_pickaxeModel(event);
 		SugarDTools.Sugar_diamond_axeModel(event);
 		SugarDTools.Sugar_diamond_hoeModel(event);
@@ -229,6 +186,16 @@ public class SugarDesertMod {
 		SugarDTools.Sugar_diamond_shovelModel(event);
 		SugarDTools.Sugar_diamond_swordModel(event);
 		SDItem.PuddingModel(event);
+		SDItem.Azuki_beenModel(event);
+		SDItem.Azuki_pasteModel(event);
+		SDItem.Azuki_been_bunModel(event);
+		SDItem.Poppy_seedModel(event);
+		SDItem.bismuth_crystalModel(event);
+		SDItem.bismuth_liquidModel(event);
+		SDItem.Sugar_diamondModel(event);
+		SDItem.Sugar_diamond_nuggetModel(event);
+		SDItem.Crushed_Sugar_CaneModel(event);
+		SDItem.Raw_SugarModel(event);
 	}
 
 	@SubscribeEvent
@@ -236,5 +203,11 @@ public class SugarDesertMod {
     public void registerBiomes(RegistryEvent.Register<Biome> event) {
 		IForgeRegistry<Biome> registry = event.getRegistry();
 		ModBiome.registerBiomes(registry);
+	}
+
+	@Mod.EventHandler
+	//一番最後に処理したいもの
+	public void postInit(FMLPostInitializationEvent event) {
+
 	}
 }
